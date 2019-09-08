c344192857dfb3c1f0f562850f2cc42f14f13849
Ryan Norby <ryan@berserkguard.com>
2019-09-08 05:03:30 -0400

    Added command to Quicksell for previewing the vendorable grays. Cleaned up code a bit for readibility.

--- plugins/Quicksell.lua
+++ plugins/Quicksell.lua
@@ -6,25 +6,37 @@ local Quicksell = {}
 --|   Quicksell Public API   |--
 --|--------------------------|--
 
-function Quicksell:VendorTrash(input)
-    local c, i, n, v = 0
-    
-    for b = 0, 4 do
-        for s = 1, GetContainerNumSlots(b) do
-            i = {GetContainerItemInfo(b,s)}
-            n = i[7]
-            
-            if n and string.find(n, "9d9d9d") then
-                v = {GetItemInfo(n)}
-                q = i[2]
-                c = c + v[11] * q
-                
-                UseContainerItem(b, s)
-                print(n, q)
+-- If 'commit' is true, attempts to sell all grays to the already-open vendor window.
+-- If 'commit' is false, then only a preview of grays and total worth is dumped to chat window.
+function Quicksell:VendorTrash(commit)
+    local totalWorth = 0
+
+    for bag = 0, 4 do
+        for slot = 1, GetContainerNumSlots(bag) do
+            local containerInfo = {GetContainerItemInfo(bag, slot)}
+            local itemLink = containerInfo[7]
+
+            -- If the item link contains the hex for the gray color, it's trash
+            if itemLink and string.find(itemLink, "9d9d9d") then
+                local sellPrice = select(11, GetItemInfo(itemLink))
+                local stackCount = containerInfo[2]
+                local stackWorth = sellPrice * stackCount
+
+                totalWorth = totalWorth + stackWorth
+
+                if commit then
+                    UseContainerItem(bag, slot)
+                end
+                print(tostring(stackCount) .. "x" .. itemLink .. ": " .. GetCoinText(stackWorth))
             end
         end
     end
-    LogMessage("Trash vendored. Total: " .. GetCoinText(c))
+
+    if commit then
+        LogMessage("Trash vendored. Total: " .. GetCoinText(totalWorth))
+    else
+        LogMessage("Total trash worth: " .. GetCoinText(totalWorth))
+    end
 end
 
 --|-------------------------|--
@@ -32,4 +44,5 @@ end
 --|-------------------------|--
 
 Daddy:RegisterPlugin("Quicksell", Quicksell)
-Daddy:RegisterSubcommand("vendor", Quicksell.VendorTrash, "Vendors all trash (grays) if a vendor window is open")
+Daddy:RegisterSubcommand("vendor", function(input) Quicksell:VendorTrash(true) end, "Vendors all trash (grays) if a vendor window is open")
+Daddy:RegisterSubcommand("trashworth", function(input) Quicksell:VendorTrash(false) end, "Evaluates the total worth of trash in bags")
